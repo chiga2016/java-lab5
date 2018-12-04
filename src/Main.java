9e07f070b463988ccc3a51afe414d4804f3c72dc
chiga2016 <chiga_@bk.ru>
2018-12-04 22:44:05 +0500

    первые 3 задания

--- src/Main.java
+++ src/Main.java
@@ -14,6 +14,7 @@ public static void main (String[] args ) throws SQLException {
     db.getGroupID("ТЕЛЕВИЗОРЫ", conn).stream()
             .forEach(p-> System.out.println(p));
 
+
     //System.out.println(db.getGroupID("ТЕЛЕВИЗОРЫ", conn).toString());
 
 //    ItemData itmd = new ItemData(1,2,"Компы");
